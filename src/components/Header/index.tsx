66276cf8739a455d99b2613da0685b80a147afe0
Renato <rnatu91@gmail.com>
2022-08-02 16:26:43 -0300

    style: change colors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import styles from './styles.module.scss';
 
 import pokeball from '../../assets/images/pokeball.svg';
-import synviaLogo from '../../assets/images/synvia-A.svg';
 
 export function Header() {
   return (
@@ -11,12 +10,6 @@ export function Header() {
         <div>
           <img src={pokeball} alt="Pokebola" />
           <h1>Pokédex</h1>
-        </div>
-        <div>
-          <img src={synviaLogo} alt="Synvia Logo" />
-          <button type="button">
-            <i className="fas fa-sign-out-alt fa-lg" />
-          </button>
         </div>
       </div>
     </header>
